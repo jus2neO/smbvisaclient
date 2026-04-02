353e3d9ea967336c0eaa04accc71221d226b4b40
Justine <jus.2nedrive1@gmail.com>
2026-04-02 19:09:24 +0800

    chore: update base URL for smbvisaclient deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/smb-react/',
+  base: '/smbvisaclient/',
   plugins: [
     react(),
     tailwindcss(),
